31ea345797c978a52fafc8d7703573d1e15cfcdd
figmoomoo <figmoomoo@gmail.com>
2020-07-22 16:34:04 -0500

    finished set 1

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+// class App extends Component {
+//   constructor(){
+//     super();
+//     this.state={
+//       foods: ['spaghetti', 'ice cream', 'sushi', 'bologna', 'cheese']
+//     }
+//   }
+
+//   render(){
+//     return(
+//       <div className="App">
+
+//       </div>
+//     )
+//   }
+// }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>spaghetti</h1>
+      <h1>ice cream</h1>
+      <h1>sushi</h1>
+      <h1>bologna</h1>
+      <h1>cheese</h1>
     </div>
   );
 }
+
 
 export default App;
